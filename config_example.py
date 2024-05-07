d588a04c1bcf72a9f31305bf228e43f071149b68
Michael Lindsay <michaeldeanlindsay@gmail.com>
2024-05-07 14:52:00 -0700

    config example updates

---
+++ config_example.py
@@ -0,0 +1,5 @@
+user = ""
+pwd = ""
+
+pna_connection_string = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=PNASQL01;DATABASE=NetSuite;UID="+user+";PWD="+pwd+";Trusted_Connection=YES"
+cag_connection_string = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=CAGMAIN;DATABASE=DATA_IMPORTS;UID="+user+";PWD="+pwd+";Trusted_Connection=YES"
